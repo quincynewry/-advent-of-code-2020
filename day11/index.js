0608262c58c0c57bb6cb0c95658dd5db0515624f
Mark Quinn <mark@playcircl.com>
2020-12-12 17:57:22 +0000

    refactor to run for both parts

--- day11/index.js
+++ day11/index.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-async function run() {
+function run(maxOccupied, mode) {
     const input = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8');
     let lines = input.split('\n');
     let previousGrid = [];
     let activeGrid = [];
+    let count = 0;
 
     lines.forEach((x) => {
         activeGrid.push(x.split(''));
@@ -24,79 +25,82 @@ async function run() {
                     continue;
                 }
 
-                const adajacentOccupiedCount = getAdjacentOccupiedCount(previousGrid, x, y);
+                const occupiedCount = getOccupiedCount(previousGrid, x, y, mode);
 
-                if (space == 'L' && adajacentOccupiedCount === 0) {
+                if (space == 'L' && occupiedCount === 0) {
                     activeGrid[x][y] = '#';
-                } else if (space == '#' && adajacentOccupiedCount >= 5) {
+                } else if (space == '#' && occupiedCount >= maxOccupied) {
                     activeGrid[x][y] = 'L';
                 }
             }
         }
     } while (JSON.stringify(previousGrid) != JSON.stringify(activeGrid));
 
-    let count = 0;
-
     activeGrid.forEach((x) => {
         count += x.filter((y) => y == '#').length;
-
-        console.log(x.join(''));
     });
 
-    console.log('Answer: ', count);
+    return count;
 }
 
-function getAdjacentOccupiedCount(rows, rowIdx, spaceIdx) {
+function getOccupiedCount(rows, rowIdx, spaceIdx, mode) {
     let count = 0;
 
     if (rows[rowIdx - 1]) {
         // top left
-        count += canSeeOccupied(rows, rowIdx, spaceIdx, -1, -1);
+        count += checkOccupied(rows, rowIdx, spaceIdx, -1, -1, mode);
 
         // above
-        count += canSeeOccupied(rows, rowIdx, spaceIdx, -1, 0);
+        count += checkOccupied(rows, rowIdx, spaceIdx, -1, 0, mode);
 
-        count += canSeeOccupied(rows, rowIdx, spaceIdx, -1, 1);
+        count += checkOccupied(rows, rowIdx, spaceIdx, -1, 1, mode);
     }
 
     // left
-    count += canSeeOccupied(rows, rowIdx, spaceIdx, 0, -1);
+    count += checkOccupied(rows, rowIdx, spaceIdx, 0, -1, mode);
 
     // right
-    count += canSeeOccupied(rows, rowIdx, spaceIdx, 0, 1);
+    count += checkOccupied(rows, rowIdx, spaceIdx, 0, 1, mode);
 
     if (rows[rowIdx + 1]) {
         // bottom left
-        count += canSeeOccupied(rows, rowIdx, spaceIdx, 1, -1);
+        count += checkOccupied(rows, rowIdx, spaceIdx, 1, -1, mode);
 
         // underneath
-        count += canSeeOccupied(rows, rowIdx, spaceIdx, 1, 0);
+        count += checkOccupied(rows, rowIdx, spaceIdx, 1, 0, mode);
 
         // bottom right
-        count += canSeeOccupied(rows, rowIdx, spaceIdx, 1, 1);
+        count += checkOccupied(rows, rowIdx, spaceIdx, 1, 1, mode);
     }
 
     return count;
 }
 
-run();
-
-function canSeeOccupied(rows, x, y, xD, yD) {
-    let count = 0;
+function checkOccupied(rows, x, y, xD, yD, mode) {
+    let counter = 0;
     x += xD;
     y += yD;
 
     while (rows[x] && rows[x][y]) {
-        if (rows[x][y] == '#' || rows[x][y] == 'L') {
+        if (mode == 'direct' && counter > 0) {
+            break;
+        }
+
+        if (rows[x][y] != '.') {
             if (rows[x][y] == '#') {
-                count++;
+                return 1;
             }
+
             break;
         }
 
         x += xD;
         y += yD;
+        counter++;
     }
 
-    return count;
+    return 0;
 }
+
+console.log('Part 1:', run(4, 'direct'));
+console.log('Part 2:', run(5, 'visible'));
